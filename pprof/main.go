c37c04985523080f272b099a10958b17f30ec606
1003818507@qq.com <1003818507@qq.com>
2021-06-04 09:22:50 +0800

    pprof

--- pprof/main.go
+++ pprof/main.go
@@ -4,14 +4,32 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"runtime"
 	"time"
 )
 var datas []string
+
+func init()  {
+	runtime.SetMutexProfileFraction(1)
+	runtime.SetBlockProfileRate(1)
+}
+
 func main()  {
+	//var m sync.Mutex
+	//var datas = make(map[int]struct{})
+	//for i:=0;i<999;i++ {
+	//	go func(i int) {
+	//		m.Lock()
+	//		defer  m.Unlock()
+	//		datas[i] = struct{}{}
+	//	}(i)
+	//}
+	//
+	//_ = http.ListenAndServe(":6061", nil)
 	go func() {
 		for  {
 			log.Printf("len : %d",Add("go-grogramming-tour-book"))
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Second * 10)
 		}
 	}()
 	_ = http.ListenAndServe("0.0.0.0:6060", nil)
